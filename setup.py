fe05eb174057e9fa3532a2b2977543e05e89e6a0
Manuel Francisco Naranjo <manuel@aircable.net>
2011-08-31 17:05:54 -0300

    build egg file, modified url, bumped version number, added readme.rst as long_description

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-from distutils.core import setup
- 
+#from distutils.core import setup
+from setuptools import setup
+import os, os.path
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name="django_cpserver",
-    version="0.1",
+    version="0.2",
     description="Management commands for serving Django via CherryPy's built-in WSGI server",
     author="Peter Baumgartner",
     author_email="pete@lincolnloop.com",
-    url="http://lincolnloop.com/blog/2008/mar/25/serving-django-cherrypy/",
+    url="https://github.com/manuelnaranjo/django-cpserver",
     packages=[
         "django_cpserver",
         "django_cpserver.management",
         "django_cpserver.management.commands",
     ],
+    license="BSD",
+    long_description=read("README.rst")
 )
